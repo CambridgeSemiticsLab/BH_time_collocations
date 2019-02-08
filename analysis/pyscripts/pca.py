1ec0e122abd40d44846686fb4b3173cdb1cd8a36
Cody Kingham <codyakingham@gmail.com>
2019-02-08 17:45:29 +0000

    add new analysis

---
+++ analysis/pyscripts/pca.py
@@ -0,0 +1,58 @@
+'''
+Module contains a useful method for plotting PCA data with tags annotated.
+'''
+
+import matplotlib.pyplot as plt
+
+def plot_PCA(pca_nouns, 
+             zoom=tuple(), 
+             noun_xy_dict=False, 
+             save='', 
+             annotate=True, 
+             title='', 
+             components=tuple(),
+             annoTags=[]):
+    '''
+    Plots a PCA noun space.
+    Function is useful for presenting various zooms on the data.
+    '''
+    
+    x, y = components
+    
+    # plot coordinates
+    plt.figure(figsize=(12, 10))
+    plt.scatter(x, y)
+
+    if zoom:
+        xmin, xmax, ymin, ymax = zoom
+        plt.xlim(xmin, xmax)
+        plt.ylim(ymin, ymax)
+    
+    if title:
+        plt.title(title, size=18)
+    plt.xlabel('PC1', size=18)
+    plt.ylabel('PC2', size=18)
+    plt.axhline(color='red', linestyle=':')
+    plt.axvline(color='red', linestyle=':')
+    
+    # annotate points
+    if annotate:
+        noun_xy = {} # for noun_dict
+        noun_lexs = annoTags
+        
+        for i, noun in enumerate(noun_lexs):
+            noun_x, noun_y = x[i], y[i]
+            noun_xy[annoTags[i]] = (noun_x, noun_y)
+            if zoom: # to avoid annotating outside of field of view (makes plot small)
+                if any([noun_x < xmin, noun_x > xmax, noun_y < ymin, noun_y > ymax]):                
+                    continue # skip noun
+            plt.annotate(noun, xy=(noun_x, noun_y), size='18')
+    
+    if save:
+        plt.savefig(save, dpi=300, bbox_inches='tight')
+    
+    
+    plt.show()
+    
+    if noun_xy_dict:
+        return noun_xy
