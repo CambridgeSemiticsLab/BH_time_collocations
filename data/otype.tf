1cc5afa30252150d7e1e690a36aba31c1fac8dbe
Cody Kingham <codyakingham@gmail.com>
2019-03-27 12:58:28 +0000

    new preprocessed data generated for analysis; lots of checks and corrections to time phrases in BHSA

---
+++ data/otype.tf
@@ -0,0 +1,22 @@
+@node
+@coreData=BHSA
+@coreVersion=c
+@created_by=Cody Kingham, Dirk Roorda, and Constantijn Sikkel (ETCBC)
+@valueType=str
+@writtenBy=Text-Fabric
+@dateWritten=2019-03-26T14:05:26Z
+
+1-426584	word
+426585-426623	book
+426624-427552	chapter
+427553-515673	clause
+515674-606361	clause_atom
+606362-651541	half_verse
+651542-904748	phrase
+904749-1172289	phrase_atom
+1172290-1236016	sentence
+1236017-1300541	sentence_atom
+1300542-1414353	subphrase
+1414354-1437566	verse
+1437567-1446799	lex
+1446800-1699934	phrase2
