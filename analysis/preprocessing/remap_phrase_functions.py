28678d2499fb1f974207a5dbf79d181e16e2f6e8
Cody Kingham <codyakingham@gmail.com>
2019-04-06 02:00:07 +0100

    continue working

---
+++ analysis/preprocessing/remap_phrase_functions.py
@@ -0,0 +1,44 @@
+'''
+-- Correcting Time Functions in BHSA -- 
+In this script, phrase functions are redrawn
+based on the manual corrections stored in the 
+dictionary named `newfunctions`
+'''
+
+import collections
+from tf.fabric import Fabric
+
+dirs = {'bhsa': '~/text-fabric-data/etcbc/bhsa/tf/c', 
+        'export': '~/github/csl/time_collocations/data'}
+
+# load TF api + functions
+TF = Fabric(locations=dirs['bhsa'], silent=True)
+api = TF.load('''
+
+function
+
+''', silent=True)
+
+# draw phrase functions from BHSA base data
+phrase2funct = dict((ph, api.F.function.v(ph)) for ph in api.F.otype.s('phrase'))
+
+# map corrections/changes here
+newfunctions = {849296:'Loca',
+                825329:'Loca',
+                828081:'Cmpl',
+                774349:'Adju',
+                774352:'Adju',
+                775948:'Adju',
+                775985:'Adju',
+                876172:'Adju',}
+
+phrase2funct.update(newfunctions)
+
+# export
+meta = {'':{'source': 'https://github.com/etcbc/bhsa',
+            'origin': 'Made by the ETCBC of the Vrije Universiteit Amsterdam; edited by Cody Kingham'},
+        'function':{'valueType':'str'}}
+TFexport = Fabric(locations=dirs['export'], silent=True)
+TFexport.save(metaData=meta, nodeFeatures={'function':phrase2funct})
+
+print('remapped functions ready...')
