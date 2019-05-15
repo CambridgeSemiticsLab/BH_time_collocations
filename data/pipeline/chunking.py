2b2ffe4c568264abbd03da9132b9c72eb3d16dba
Cody Kingham <codyakingham@gmail.com>
2019-05-15 17:25:44 +0100

    update pipeline

--- data/pipeline/chunking.py
+++ data/pipeline/chunking.py
@@ -24,16 +24,16 @@ class Chunker:
         # NB that new function data is loaded from output_dir
         locas = [bhsa_dir, heads_dir, output_dir, metadata]
         TF = Fabric(locations=locas, silent=True)
-        load_features = ['function', 'note', 'sem_set', 'head', 'nhead']
+        load_features = ['function', 'note', 'sem_set', 'head', 'nhead',
+                         'st', 'ls']
         self.bhsa = bhsa = self.TF.load(' '.join(load_features))        
         
         # configure counters and dicts to build/store new objects
         self.nodeFeatures = collections.defaultdict(lambda:collections.defaultdict())
         self.edgeFeatures = collections.defaultdict(lambda:collections.defaultdict())
         
-        # build BHSA otype, oslots, and new function & note features
-        # new objects will be generated on top of these
-        # but all are needed in order to export the new oslots.tf and otype.tf files
+        # gather BHSA otype and oslots (+function & note)
+        # new objects/features will be generated on top of these
         self.nodeFeatures['otype'] = dict((n, bhsa.F.otype.v(n)) for n in bhsa.N())
         self.edgeFeatures['oslots'] = dict((n, bhsa.L.d(n, 'word')) for n in bhsa.N()\ 
                                                if bhsa.F.otype.v(n) != 'word')
@@ -45,16 +45,14 @@ class Chunker:
         # new node numbers calculated from here
         self.newNode = max(self.nodeFeatures['otype'].keys())
         
-        
     def chunk_time(self):
         '''
         There are several cases in BHSA where time phrases 
         are divided into several pieces whereas elsewhere 
         the parts are kept together as a single phrase. 
         This is an undesirable inconsistency. To solve this problem, 
-        new phrase boundaries are generated and mapped over the old 
-        boundaries stored in the oslots file. 
-        
+        new chunk objects are generated and mapped over the two phrase
+        boundaries. The objects have a label=timephrase.
         This method operates on self.edgeFeatures and self.nodeFeatures
         to add new objects.
         '''
@@ -70,9 +68,8 @@ class Chunker:
         # by another time since these will be subsumed into the first time.
         for phrase in F.function.s('Time'):
             
-            # assign features if needed
+            # assign chunk boundaries here
             chunkSlots = []
-            chunkRoles = {} # wordnode that is time mapped to this chunk
             
             # examine positions around time phrase
             thisclause = L.d(L.u(phrase, 'clause')[0], 'phrase') # all phrases in cl
@@ -112,22 +109,163 @@ class Chunker:
             for time in E.head.t(phrase):
                 self.edgeFeatures['role'][time] = {phrase:'time'}
     
+    def get_item(self, item, iterable, default=0):
+        '''
+        Checks if item is in an iterable.
+        If present, return item. If absent, return default.
+        Good for selecting +/- slots in a clause/phrase tuple.
+        '''
+        if item in iterable:
+            return item
+        else:
+            return default
+        
+    def fill_slots(chunk):
+        '''
+        Fills in gapped slots such as waws and other
+        items that are missing in a quant chunk (see below).
+        '''
+        chunk.sort()
+        minSlot, maxSlot = chunk[0], chunk[-1]
+        return list(range(minSlot, maxSlot+1))
         
     def chunk_quants(self):
         '''
         In Hebrew, cardinal numbers are split
-        into several parts, e.g. 
-        חמשׁ ועשרים יום
-        == "five and twenty days"
-        For this project, I want to be able to easily
-        navigate and filter through number chains.
-        This module does that by chunking numbers into
-        individual objects. That allows more complex chunks
-        to be handled easily.
+        into several parts, e.g. חמשׁ ועשרים יום
+        == "five and twenty days" For this project, 
+        I want to be able to easily select and label 
+        number chains. This method does that by 
+        chunking numbers into chunk objects.         
+        The chunks are built in three steps:
+        1. quantifier "atoms" are isolated, which consist of
+            the following pairs:
+            * cardinal + noun
+            * noun + cardinal
+            * cardinal + ה + noun
+            * cardinal + ו + cardinal
+            * cardinal + cardinal
+            These pairs account for all of the atomic parts
+            of a quantifier chunk.
+        2. combine quantifier atoms by isolating continuous
+            runs. These are basic quant chunks.
+        3. combine adjacent quant chunks to capture larger,
+            composite chains: e.g. שבע שנה ומאת שנח.
         '''
         
-    
+        # Gather qantifier atoms
+        quant_atoms = []
+        cardinals = [word for lex in F.ls.s('card')
+                        for word in L.d(lex, 'word')
+                        if F.language.v(word) == 'Hebrew']
         
+        for number in cardinals:
+            
+            # get positions above and around number
+            phrase = L.d(L.u(number, 'phrase')[0], 'word')
+            phrase_atom = L.d(L.u(number, 'phrase_atom')[0], 'word')
+            p1 = get_item(number+1, phrase, 0) # plus 1 slot, etc.
+            p2 = get_item(number+2, phrase, 0) 
+            m1 = get_item(number-1, phrase, 0) # minus 1, etc.
+            m2 = get_item(number-2, phrase, 0)
+            m1pa = get_item(number-1, phrase_atom, 0) # minus 1 in phrase atom
+            subs = {'subs', 'nmpr', 'prps'} # valid POS for quantified objects
+            
+            # match atoms
+            # card + noun
+            if all([F.pdp.v(p1) in subs,
+                    F.ls.v(p1) != 'card',
+                    F.sem_set.v(p1) != 'prep']):
+                quant_atoms.append((number, p1))
+                
+            # noun + card
+            elif all([F.pdp.v(m1pa) in subs,
+                      F.ls.v(m1pa) != 'card',
+                      F.sem_set.v(m1pa) != 'prep',
+                      F.st.v(m1pa) == 'a']):
+                quant_atoms.append((m1pa, number))
+            
+            # card + ה + noun
+            elif all([F.lex.v(p1) == 'H',
+                      F.pdp.v(p2) in subs,
+                      F.ls.v(p2) != 'card']):
+                quant_atoms.append((number, p2))
+                
+            # card + ו + card
+            elif all([F.lex.v(p1) == 'W',
+                      F.ls.v(p2) == 'card']):
+                quant_atoms.append((number, p2))
+                
+            # card + card
+            elif F.ls.v(p1) == 'card':
+                quant_atoms.append((number, p1))
+                
+            # ø + card + ø
+            elif not any([F.lex.v(m1) == 'W' and F.ls.v(m2) == 'card', 
+                          F.ls.v(m1) == 'card', 
+                          F.lex.v(number)=='>XD/']):
+                quant_atoms.append((number,))
+                
+        quant_atoms = sorted(quant_atoms) # sort chunks for processing
+                
+        # link atoms on the last number in the chains
+        # e.g. (3800, 3802), (3802, 3803), (3803, 3804)
+        # links should be made with 3802 and 3803, which
+        # unite the three tuples. We do this with a dict.
+        # The key is the last item in the tuple, the value
+        # is a list of the tuple's contents. 
+        # We use dict.pop() when a tuple's first
+        # item matches another's last item, the key, 
+        # and we pop the value of the first & combine the 2
+        qchunks = {}
+        for atom in quant_atoms:
+            # match first slot to last slot in an atome
+            if atom[0] in qchunks:
+                qchunks[atom[-1]] = qchunks.pop(atom[0]) + list(atom)[1:]
+            # map atoms to dict
+            else:
+                qchunks[atom[-1]] = list(atom)
+        
+        qchunks = list(qchunks.values()) # take the chunks
+        
+        # link chunks contained in same phrase_atom
+        # this includes cases where two nouns are quantified
+        # but both quant chunks are intended to be read together
+        # as a sigle set. Often the two nouns are the same, but not
+        # always, e.g. "forty days and forty nights."
+        
+        # make phrase_atom 2 chunk mapping
+        phrase2chunks = collections.defaultdict(list)
+        for chunk in qchunks:
+            phrase_atom = L.u(chunk[0], 'phrase_atom')[0]
+            phrase2chunks[phrase_atom].append(chunk)
+            
+        # find composite chunks
+        phrase, chunks in phrase2chunks.items():
+            # skip simple chunks
+            if len(chunks) < 2:
+                continue
+            # get the quantified nouns
+            chunknouns = [word for chunk in chunks for word in chunk
+                              if F.ls.v(word) != 'card']
+            # look for composite quantification
+            if len(chunknouns) < 2:
+                continue
+            # add composite chunk
+            qchunks.append([word for chunk in chunks for word in chunk])
+        
+        # generate the chunk objects
+        for chunk in qchunks:
+            chunkslots = fill_slots(chunk) # fill in gaps
+            chunknouns = [word for word in chunk if F.ls.v(word) != 'card']
+            chunkquants = [word for word in chunk if F.ls.v(word) == 'card']
+            label = 'quant_NP' if chunknouns else 'quant'
+            self.newNode += 1
+            self.edgeFeatures['oslots'][self.newNode] = chunkslots
+            self.nodeFeatures['otype'][self.newNode] = 'chunk'
+            self.nodeFeatures['label'][self.newNode] = label
+            
+    
     def execute(self):
         '''
         Orders and runs all chunking methods
