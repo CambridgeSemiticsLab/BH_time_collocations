82e6c4ab52a8b53db4aa83070b2fcd750687cb82
Cody Kingham <codyakingham@gmail.com>
2019-05-17 16:25:16 +0100

    updates

--- data/tf/old/steps.tf
+++
@@ -1,9 +0,0 @@
-@config
-@coreData=BHSA
-@coreVersion=c
-@origin=Made by the ETCBC of the Vrije Universiteit Amsterdam; edited by Cody Kingham
-@source=https://github.com/etcbc/bhsa
-@valueType=int
-@writtenBy=Text-Fabric
-@dateWritten=2019-04-24T09:01:29Z
-
