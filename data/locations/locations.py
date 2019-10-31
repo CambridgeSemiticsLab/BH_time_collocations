f2aa26a1d47586e99aabf8ef4de1dfd54846b3c1
Cody Kingham <codyakingham@gmail.com>
2019-10-31 18:32:23 +0000

    Change var name and fix indents

---
+++ data/locations/locations.py
@@ -0,0 +1,13 @@
+# Text-Fabric data locations for the project
+
+import os.path as path
+
+home = path.expanduser('~')
+repo = path.expanduser('~/github/CambridgeSemiticsLab/time_collocations')
+custom_data = path.join(repo, 'data/text_fabric/tf')
+bhsa_data = path.join(home, 'text-fabric-data/etcbc/bhsa/tf/c')
+
+data = {
+    'bhsa': bhsa_data,
+    'custom': custom_data
+}
