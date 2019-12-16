0c8cea263df7e0351f77b5a660648a7fe4916763
Cody Kingham <codyakingham@gmail.com>
2019-12-16 14:54:24 +0000

    Begin re-organizing and restructuring TF mods with CV tools and updated methods

---
+++ data/bhsa/modify.py
@@ -0,0 +1,52 @@
+import collections
+from tf.fabric import Fabric
+from tf.compose import modify
+from weqetal import convert_tense
+
+def mod_features(locations, metadata):
+    """Remap node features in BHSA
+
+    BHSA contains features of  nodes that 
+    are sometimes inaccurate or not quite
+    fit for purpose for this study. In this
+    module, we modify the dataset with Text-Fabric
+    and export the new version to a new directory.
+    """
+
+    # setup data and methods
+    bhsa, output = locations['bhsa'], locations['output']
+    TF = Fabric(locations=bhsa, silent=True)
+    api = TF.load('vt mother pdp lex')
+
+    # list node features to be modified
+    to_modify = {
+        'nodeFeatures': {
+            'function': {
+                849296:'Loca', 
+                825329:'Loca',
+                828081:'Cmpl',
+                774349:'Adju',
+                774352:'Adju',
+                775948:'Adju',
+                775985:'Adju',
+                876172:'Adju',
+                881665:'Objc', # phrase belongs with previous as adjectival element
+                870273:'Time', # prep and conj belong with time phrase
+                870274:'Time', # modifier "KBR" belongs with time phrase
+            },
+           'vt': {
+            }
+        },
+    }
+
+    # modify verb tenses to add weqetal
+    for verb in api.F.pdp.s('verb'):
+        to_modify['nodeFeatures']['vt'][verb] = convert_tense(verb, api)
+
+    # enact the changes
+    modify(
+        locations['bhsa'],
+        locations['output'],
+        addFeatures=to_modify,
+        featureMeta={feat:metadata for feat in ('function','vt')}
+    )
