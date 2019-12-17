e67efcf97076df32100128a37dd8110a47cd3cd0
Cody Kingham <codyakingham@gmail.com>
2019-12-17 16:05:05 +0000

    Integrate with central tf_data paths

--- data/bhsa/generate.py
+++ data/bhsa/generate.py
@@ -4,22 +4,14 @@ BHSA Hebrew database.
 """
 
 from pathlib import Path
+from paths import tf_data
 from datetime import datetime
 from tf.fabric import Fabric
 from modify import mod_features
 #from time_association import TimeAssociation
 
-home = Path.home()
-bhsa_dir = Path(home, 'text-fabric-data/etcbc/bhsa/tf/c')
-output_dir = Path('tf_test')
-
-locations = {
-    'bhsa': str(bhsa_dir),
-    'output': str(output_dir),
-}
-
 base_metadata = {
-    'modified_by': 'Modifications added by Cody Kingham', 
+    'modified': 'Modifications added by Cody Kingham', 
 }
 
 start = datetime.now()
@@ -35,7 +27,7 @@ def msg(m, indent=0):
 # -- Remap Node Features --
 msg('Remapping TF features...')
 print('*'*20)
-mod_features(locations, base_metadata)
+mod_features(tf_data, base_metadata)
 print('*'*20)
 msg('done!', 1)
 
