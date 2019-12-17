5e1881888e85ab132cee6254a95c636d2c1a6169
Cody Kingham <codyakingham@gmail.com>
2019-12-17 15:57:39 +0000

    Add path import

--- tools/tf_tools/load.py
+++ tools/tf_tools/load.py
@@ -3,15 +3,9 @@ Sets up an instance of TF along by
 loading standard features + custom ones.
 '''
 
-import os.path as path
+from paths import tf_data
 from tf.app import use
 from tf.fabric import Fabric
-
-home = path.expanduser('~')
-repo = path.expanduser('~/github/CambridgeSemiticsLab/time_collocations')
-custom_data = path.join(repo, 'data/bhsa/tf')
-bhsa_data = path.join(home, 'text-fabric-data/etcbc/bhsa/tf/c')
-tf_data = [bhsa_data, custom_data]
 
 standard = ''' 
 pdp vs vt
@@ -29,9 +23,11 @@ top_assoc
 '''
 default = standard + plus
 
+locations = list(tf_data.values())
+
 def load_tf(features='', silent=False):
     """Load an instance of TF with standard and other features"""
-    TF = Fabric(locations=tf_data, silent=silent)
+    TF = Fabric(locations=locations, silent=silent)
     features = default + features
     api = TF.load(features, silent=silent)
     A = use('bhsa', api=api, silent=True)
