05f2e4a4ebc09d3736c8bbc133ef831dd013013f
Cody Kingham <codyakingham@gmail.com>
2020-01-07 16:37:06 +0000

    Add feature option

--- tools/tf_tools/tokenizers.py
+++ tools/tf_tools/tokenizers.py
@@ -3,18 +3,16 @@ This module contains functions for tokenizing
 the surface forms of time adverbials.
 '''
 
-def tokenize_surface(nodes, tf):
+def tokenize_surface(nodes, tf, feature='g_cons_utf8', sep='.'):
     '''
     Return a surface token string of a BHSA node.
     The words are dot-separated and heh consonants
     are added if they are present in vocalized form. 
     '''
-    F, L = tf.F, tf.L
+    Fs, L = tf.Fs, tf.L
     subtokens = []
     slots = L.d(nodes,'word') if type(nodes) == int else nodes
     for s in slots:
-        if F.lex.v(s) == 'H':
-            subtokens.append('ה')
-        else:
-            subtokens.append(F.g_cons_utf8.v(s))
-    return '.'.join(subtokens)
+        lex = L.u(s, 'lex')[0]
+        subtokens.append(Fs(feature).v(lex))       
+    return f'{sep}'.join(subtokens)
