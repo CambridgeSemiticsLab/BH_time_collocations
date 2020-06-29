4a486c727e24ceaa91c0768205c83537718f9f4d
Cody Kingham <codyakingham@gmail.com>
2020-06-29 21:25:51 +0100

    Add updated analyses

--- results/notebooks/advb_article/get_hmod_sample.py
+++ results/notebooks/advb_article/get_hmod_sample.py
@@ -1,2 +1,2 @@
 from __main__ import *
-head_mod_df = functs_df[~((functs_df.head_type == 'prep') & (functs_df.suffix))]
+hm_df = functs_df[~((functs_df.head_type == 'prep') & (functs_df.suffix))].copy()
