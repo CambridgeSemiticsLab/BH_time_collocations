7a0d9660513ce2d73000b9e6ce1364c2bed4f24f
Cody Kingham <codykingham@icloud.com>
2023-05-06 11:44:58 -0500

    Refactor

--- data/pipeline/scripts/corpus_building/config.py
+++ data/pipeline/scripts/corpus_building/config.py
@@ -36,90 +36,102 @@ THESIS_CORPUS_PARAMS = dict(
     ],
     update_features={
         "function": {
-            # Adju->Time
-            # Exod 12:18; mislabeled as Adju
-             673370: "Time",
+
+            # Subj->Time
+            # Gen 1:5; based on other wayehi-x clauses with
+            # time phrases, this function appears to be mislabeled...
+            # it's not that "evening was"...there is a dummy subject;
+            # so: "*it* was evening"
+            651617: "Time",
+
+            # Subj-> Time
+            # Gen 1:5; see note on 651617
+            651620: "Time",
 
             # Adju->Time
             # Exod 12:18; mislabeled as Adju
-             673373: "Time",
+            673370: "Time",
+
+            # Adju->Time
+            # Exod 12:18; mislabeled as Adju
+            673373: "Time",
 
             # Adju->Time
             # Exod 12:18; mislabled as Adju
-             673378: "Time",
+            673378: "Time",
 
             # Adju->Time
             # Deut 31:10; mislabled as Adju (חג)
-             714507: "Time",
+            714507: "Time",
 
             # Time->Time
             # 1 Sam 25: 15; mislabled as Loca
-             741508: "Time",
+            741508: "Time",
 
             # Time->Modi
             # 1 Sam 1:23; no reviewed sources took this as temporal
-             731946: "Modi",
+            731946: "Modi",
 
             # Time->PreC
             # Gen 9:29; phrase is pred. complement
-             654059: "PreC",
+            654059: "PreC",
 
             # Time->Adju
             # Josh 4:18; waters flowed 'as before'; not temporal
-             716817: "Adju",
+            716817: "Adju",
 
             # Time->Adju
             # 2 Kgs 13:5; 'as before'; not temporal
-             769441: "Adju",
+            769441: "Adju",
 
             # Time->Adju
             # 1 Sam 19:7; 'as before'; not temporal
-             738950: "Adju",
+            738950: "Adju",
 
             # Time->Adju
             # 1 Sam 21:6; 'as before'; not temporal
-             739973: "Adju",
+            739973: "Adju",
 
             # Time->Adju
             # Exod 30:10; This is frequentive not time location / duration
-             679412: "Adju",
+            679412: "Adju",
 
             # Time->Adju
             # Exod 30:10; This is frequentive not time location / duration
-             679414: "Adju",
+            679414: "Adju",
 
             # Time->Adju
             # Lev 16:34; This is frequentive not time location / duration
-             688372: "Adju",
+            688372: "Adju",
 
             # Time->Adju
             # Lev 23:16; how far one should count, not temporal
-             690340: "Adju",
+            690340: "Adju",
 
             # Time->Adju
             # Lev 26:18; No transs interprets as temporal
-             691647: "Adju",
+            691647: "Adju",
 
             # Time->Adju
             # Num 2:31; 'set out last', this is sequential but not with respect to timeline
-             692794: "Adju",
+            692794: "Adju",
 
             # Time->Cmpl
             # Josh 8:14; most take as locative; LXX omits; though Targum reads as ZMN, 'time'
-             718111: "Cmpl",
+            718111: "Cmpl",
 
             # Time->Adju
             # 1 Sam 18:10; comparative not temporal
-             738537: "Adju",
+            738537: "Adju",
 
             # Time->PreC
             # Num 28:14; the phrase belongs to prev phrase as part of genitive; 'חדש בחדו' is the
             # complete idiom; =burnt offfering of each month
-             701713: "PreC",
+            701713: "PreC",
 
             # Time->Freq
             # 1 Kgs 10:22; this frequentive explains some L+time cx
-             757981: "Freq",
+            757981: "Freq",
         },
     },
     update_metadata={
